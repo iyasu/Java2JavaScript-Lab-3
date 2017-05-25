a8b5c6c2b5631ac789680572b4942ad60275b44c
ant <anthonyjones@zipcoder3.local>
2017-05-25 10:14:20 -0400

    Cleaned up lab3

--- js/script.js
+++ js/script.js
@@ -2,13 +2,11 @@
 
 var display = document.getElementById("display");
 
-
 function assignment1() {
 
     var characters = ["a", "b", "c"];
     var numbers = [1, 2, 3];
     var newFilledArray = [];
-
     for (var i = 0; i < characters.length; i++) {
         newFilledArray.push(characters[i]);
         newFilledArray.push(numbers[i]);
@@ -16,20 +14,28 @@ function assignment1() {
     display.innerHTML = newFilledArray;
 }
 
-function assignment2() {
-    var numbers = new Array(50, 9, 2, 1, );
+function adder(){
+    
+    var numbers = prompt("Add any amount of numbers separated by commas");
+    
+    assignment2(numbers.split(","));
+}
+
+function assignment2(numbers) {
+   // var numbers = Array(50, 9, 2, 1);
     var numCatcher = "";
     var numHolder = 1;
     for (var i = 0; i < 1000; i++) {
-        var c = Math.floor(Math.random() * numbers.length);
-        var index = numbers.indexOf(c);
-        var a = numbers.splice(c, 1);
-        numbers.unshift(a)
-        var sark = numbers.join("");
-        if (parseInt(sark) > numHolder) {
-            numHolder = parseInt(sark);
+        var randomNumber = Math.floor(Math.random() * numbers.length);
+        var index = numbers.indexOf(randomNumber);
+        var splicedNumber = numbers.splice(randomNumber, 1);
+        numbers.unshift(splicedNumber)
+        var joinNumber = numbers.join("");
+        if (parseInt(joinNumber) > numHolder) {
+            numHolder = parseInt(joinNumber);
             numHolder.toString;
         }
     }
-    display.innerHTML = "An array of numbers which contains 50, 9, 2 and 1, the largest number you can generate is <br>"+numHolder;
+   display.innerHTML = "An array of numbers which contains "+ numbers+ ". The largest number you can generate is: <br>" + numHolder;
+    // display.innerHTML = numHolder;
 }
