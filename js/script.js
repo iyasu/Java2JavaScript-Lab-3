3e1121dd0393dc3d3157ed8acaf603a672ce0e71
Luis Garcia <pacalyps23@frontier.com>
2017-05-25 10:28:47 -0400

    finished lab 3

--- js/script.js
+++ js/script.js
@@ -2,12 +2,53 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+function Assignmetn1()
+{
+  var numList = [1,2,3];
+  var charList = ['a', 'b', 'c'];
+  var resultList = [];
+  for(var i =0; i < numList.length; i++)
+  {
+    resultList.push(charList[i]);
+    resultList.push(numList[i]);
+  }
+  display.innerHTML+="[";
+  display.innerHTML+=resultList.toString();
+  display.innerHTML+="]";
 }
 
-function example(){
-  var v = 3 +4;
-  
-  display.innerHTML = v;
+function Assignment2(arr)
+{
+  arr.sort();
+  display.innerHTML = arr.toString();
+  for(i = 0; i < arr.length-1; i++)
+  {
+    var temp;
+    for(j = 0; j < arr.length-1; j++)
+    {
+      if(singleDigit(arr[j]) < singleDigit(arr[j+1]))
+      {
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+
+    }
+  }
+      display.innerHTML+="<br>"
+      display.innerHTML+=arr.toString()+"<br>";
+      for(i =0; i < arr.length; i++)
+      {
+        display.innerHTML+=arr[i];
+      }
+}
+
+function singleDigit(num)
+{
+  if(num > 9)
+    return num/10;
+  else if(num > 99)
+    return num/100;
+  else
+    return num;
 }
