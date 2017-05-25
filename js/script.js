0549c7f835c575559742ef3bb45e4e08994e82fc
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-05-25 09:14:15 -0400

    first part

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+function assigntment3Point1(){
+  var numArray = [1,2,3,4,5,6,7,8];
+  var letterArray= ["a","b","c","d","e","f","e","g"];
+  var tempArr = [];
+    for (var i = 0; i < numArray.length; i++) {
+        tempArr.push(numArray[i],letterArray[i]);
+    }
+  display.innerHTML = tempArr;
 }
 
 function example(){
   var v = 3 +4;
-  
+
   display.innerHTML = v;
 }
