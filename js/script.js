95e3a33e0a1ce098ecf7f2b4955d9d0b76238134
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-05-25 09:15:23 -0400

    Finished lab 3

--- js/script.js
+++ js/script.js
@@ -2,12 +2,31 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+function yourFunctionName (arr1, arr2){
+    var newArr = [];
+    for(i = 0; i < arr1.length && arr2.length; i++){
+        newArr.push(arr1[i]);
+        newArr.push(arr2[i]);
+    }
+    //display.innerHTML = newArr;
+    return newArr;
+   
 }
 
-function example(){
-  var v = 3 +4;
-  
-  display.innerHTML = v;
+function example(arr){
+    var str = "";
+    for(i = 0; i < arr.length; i++){
+        str += arr[i];    
+    }
+    for(i =0; i < str.length; i++){
+        arr[i] = str.charAt(i);
+    }
+    arr = arr.sort().reverse()
+    str = "";
+    for(i = 0; i < arr.length; i++){
+        str += arr[i];    
+    }
+    
+    //display.innerHTML = str;
+    return str;
 }
