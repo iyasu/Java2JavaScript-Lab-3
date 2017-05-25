080878b7c6202c16a95d1d09225e6e08be0fc31d
Matt Wu <mingzhi.wu12@gmail.com>
2017-05-25 16:46:03 -0400

    finished

--- js/script.js
+++ js/script.js
@@ -8,18 +8,31 @@ function zipArrays() {
     var input2 = prompt("Please enter your second array.");
     var arr1 = input1.replace(/[^0-9]/g,'').split("");
     var arr2 = input2.replace(/[^0-9]/g,'').split("");
-    var result = [];
-    var maxLength = (arr1.length >= arr2.length) ?arr1.length :arr2.length;
 
-    for (var i = 0; i < maxLength; i++) {
-        if (arr1[i] != null){
-            result.push(arr1[i]);
+    if(arr1.length >= arr2.length) {
+        for(var i = 1; i < arr2.length; i++){
+          arr1.splice(i, 0, arr2[i-1]);
         }
-        if (arr2[i] != null){
-            result.push(arr2[i]);
+        display.innerHTML = "Your combined array is: [" + arr1 + "]";
+    }else{
+        for(var i = 0; i < arr1.length; i++){
+          arr2.splice(0, 0, arr2[i]);
         }
+        display.innerHTML = "Your combined array is: [" + arr2 + "]";
     }
-    display.innerHTML = "Your combined array is: [" + result + "]";
+}
+    // var result = [];
+    // var maxLength = (arr1.length >= arr2.length) ?arr1.length :arr2.length;
+    //
+    // for (var i = 0; i < maxLength; i++) {
+    //     if (arr1[i] != null){
+    //         result.push(arr1[i]);
+    //     }
+    //     if (arr2[i] != null){
+    //         result.push(arr2[i]);
+    //     }
+    // }
+    //display.innerHTML = "Your combined array is: [" + result + "]";
 }
 
 function largestPossibleNumber(){
