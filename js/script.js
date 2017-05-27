c15b0484af1a3ee9dddfcf3c93a2aab2a83ddd28
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-27 17:14:54 -0400

    Assignment 1 done

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@ function yourFunctionName (){
 
 function example(){
   var v = 3 +4;
-  
+
   display.innerHTML = v;
+}
+
+function zipper(){
+  var letters = ['a', 'b', 'c'];
+  var numbers = ['1', '2', '3'];
+  var zipper = [];
+  for (var i = 0; i < letters.length; i++){
+    zipper.push([letters[i], numbers[i]]);
+  }
+  display.innerHTML = zipper;
 }
