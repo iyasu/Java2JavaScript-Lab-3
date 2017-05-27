28f026ba44265d7fbeddb61668e6e0ac345cd1a5
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-27 18:02:15 -0400

    FILE DONE

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
-}
+// function yourFunctionName (){
+//   display.innerHTML = "hello";
+// }
 
-function example(){
-  var v = 3 +4;
-
-  display.innerHTML = v;
-}
+// function example(){
+//   var v = 3 +4;
+//
+//   display.innerHTML = v;
+// }
 
 function zipper(){
   var letters = ['a', 'b', 'c'];
@@ -20,4 +20,15 @@ function zipper(){
     zipper.push([letters[i], numbers[i]]);
   }
   display.innerHTML = zipper;
+}
+
+function makeLargeNumber(){
+  var randomNumbers = [50, 2, 1, 9];
+  var largeNumber = "";
+  randomNumbers.sort();
+
+  for(var i = randomNumbers.length-1; i >= 0; i--){
+    largeNumber += randomNumbers[i].toString();
+  }
+  display.innerHTML = largeNumber;
 }
